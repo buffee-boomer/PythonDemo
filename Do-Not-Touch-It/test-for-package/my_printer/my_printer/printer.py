17dd2fb6e7cba18280302d630ed8ac8cca23f09b
buffee03 <buffee0311@163.com>
2024-12-18 23:10:10 +0800

    Empty

---
+++ Do-Not-Touch-It/test-for-package/my_printer/my_printer/printer.py
@@ -0,0 +1,3 @@
+def print_string(input_string):
+    """打印传入的字符串内容"""
+    print(input_string)
---
+++ Do-Not-Touch-It/test-for-package/my_printer/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_printer',
+    version='0.1',
+    packages=find_packages(),
+    description='一个简单的字符串打印包',
+)
